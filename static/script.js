4e7a8335656676812e1c4e6ea098a4007a86b6db
Bryce MacInnis <brycemacinnis@Bryces-MacBook-Pro.local>
2019-05-16 15:26:29 -0300

    First commit.

---
+++ static/script.js
@@ -0,0 +1,25 @@
+document.getElementById("moreInfoButton").onclick = function() {
+	document.querySelector('#moreInfo').scrollIntoView({ 
+  		behavior: 'smooth' 
+	});
+}
+
+document.getElementById("contactUsButton").onclick = function() {
+	document.querySelector('#contactUs').scrollIntoView({ 
+  		behavior: 'smooth' 
+	});
+}
+
+document.getElementById("accountsButton").onclick = function() {
+	document.querySelector('#accounts').scrollIntoView({ 
+  		behavior: 'smooth' 
+	});
+}
+
+document.getElementById("faqButton").onclick = function() {
+	document.querySelector('#faq').scrollIntoView({ 
+  		behavior: 'smooth' 
+	});
+}
+
+
